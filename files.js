4fe86f1a43f89e3bc41768ee37e76e493e89f1e4
Victor Ponce <vponce@uach.mx>
2023-02-16 15:26:01 -0600

    Prueba inicial de node

---
+++ files.js
@@ -0,0 +1,24 @@
+
+//File System
+// https://nodejs.org/dist/latest-v19.x/docs/api/fs.html
+
+const fs = require('fs');
+
+console.clear()
+console.log("=============TABLA DEL 5 ==========")
+
+const base = 5;
+let salida = ''
+
+for(let i = 1; i<=10;i++){
+    salida += `${base} x ${i} = ${base*i} \n`
+}
+
+
+//console.log(salida)
+
+
+fs.writeFile('Tabla_5.txt', salida, (err) => {
+    if (err) throw err;
+    console.log('El archivo fue creado!');
+  });
